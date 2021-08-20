a9184ec4fa1524bc815b99effd2b49922adeabb9
Libranner <libranner@gmail.com>
2021-08-20 22:18:52 +0200

    Initial Commit

---
+++ TopCoder/TopCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopCoderApp.swift
+//  TopCoder
+//
+//  Created by Libranner Leonel Santos Espinal on 20/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TopCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
